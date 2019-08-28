59207ef2c9d0ceebf9ee33c250024976dd722cc6
kopok2 <oleszek.karol@gmail.com>
2019-08-28 20:55:00 +0000

    Dynamic programming problems

--- DynamicProgramming/NonDecreasingNDigits.py
+++ DynamicProgramming/NonDecreasingNDigits.py
@@ -5,5 +5,16 @@
 def ndnd(n):
     dp = [[0] * 10 for x in range(n + 1)]
     for i in range(10):
-        dp[i][1] = 1
-    for digit in range
+        dp[1][i] = 1
+    for digit in range(10):
+        for l in range(2, n + 1):
+            for x in range(digit + 1):
+                dp[l][digit] += dp[l - 1][x]
+    cnt = 0
+    for i in range(10):
+        cnt += dp[n][i]
+    return cnt
+
+
+if __name__ == "__main__":
+    print(ndnd(3))
